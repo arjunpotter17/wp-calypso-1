7535a0a3d5669fef79209111bdd65a8451b155aa
Markus Dobmann <m.do@posteo.net>
2023-03-13 13:44:30 +0100

    chore/client: migrate query keys (js,jsx,ts,tsx)

--- client/data/geo/use-geolocation-query.js
+++ client/data/geo/use-geolocation-query.js
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 export const useGeoLocationQuery = () =>
-	useQuery( 'geo', () =>
+	useQuery( [ 'geo' ], () =>
 		globalThis
 			.fetch( 'https://public-api.wordpress.com/geo/' )
 			.then( ( response ) => response.json() )
